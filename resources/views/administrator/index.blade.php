8f5f3faabe1f4c13fc61008c3cd4a0558449f6b2
MUHAMMAD ZIKRI BIN KASHIM <zikrimzk@gmail.com>
2024-11-04 16:10:10 +0800

    Setting up default design
    Add an assets folder, set up the layout for the administration page, and create a sample dashboard page as an initial development stage.

---
+++ resources/views/administrator/index.blade.php
@@ -0,0 +1,37 @@
+@extends('administrator.layouts.main')
+
+@section('content')
+ <!-- [ Main Content ] start -->
+ <div class="pc-container">
+    <div class="pc-content">
+      <!-- [ breadcrumb ] start -->
+      <div class="page-header">
+        <div class="page-block">
+          <div class="row align-items-center">
+            <div class="col-md-12">
+              <ul class="breadcrumb">
+                <li class="breadcrumb-item" aria-current="page">Dashboard</li>
+              </ul>
+            </div>
+            <div class="col-md-12">
+              <div class="page-header-title">
+                <h2 class="mb-0">Dashboard</h2>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- [ breadcrumb ] end -->
+
+
+      <!-- [ Main Content ] start -->
+      <div class="row">
+        {{-- Code Start Here ... --}}
+        
+      </div>
+      <!-- [ Main Content ] end -->
+    </div>
+  </div>
+  <!-- [ Main Content ] end -->
+
+@endsection
---
+++ resources/views/administrator/layouts/sidebar.blade.php
@@ -0,0 +1,100 @@
+<nav class="pc-sidebar">
+    <div class="navbar-wrapper">
+        <div class="m-header">
+            <a href="../dashboard/index.html" class="b-brand text-primary">
+                <!-- ========   Change your logo from here   ============ -->
+                <img src="../assets/images/logo-dark.svg" class="img-fluid logo-lg" alt="logo" />
+            </a>
+        </div>
+        <div class="navbar-content">
+            <div class="card pc-user-card">
+                <div class="card-body">
+                    <div class="d-flex align-items-center">
+                        <div class="flex-shrink-0">
+                            <img src="../assets/images/user/avatar-1.jpg" alt="user-image"
+                                class="user-avtar wid-45 rounded-circle" />
+                        </div>
+                        <div class="flex-grow-1 ms-3 me-2">
+                            <h6 class="mb-0">Zikri Mzk</h6>
+                            <small>Administrator</small>
+                        </div>
+                        <a class="btn btn-icon btn-link-secondary avtar" data-bs-toggle="collapse"
+                            href="#pc_sidebar_userlink">
+                            <svg class="pc-icon">
+                                <use xlink:href="#custom-sort-outline"></use>
+                            </svg>
+                        </a>
+                    </div>
+                    <div class="collapse pc-user-links" id="pc_sidebar_userlink">
+                        <div class="pt-3">
+                            <a href="#!">
+                                <i class="ti ti-user"></i>
+                                <span>My Account</span>
+                            </a>
+                            <a href="#!">
+                                <i class="ti ti-power"></i>
+                                <span>Logout</span>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- SideBar Menu Item Start -->
+            <ul class="pc-navbar">
+
+                <li class="pc-item pc-caption">
+                    <label>Main</label>
+                </li>
+                <li class="pc-item">
+                    <a href="##" class="pc-link">
+                        <span class="pc-micon">
+                            <svg class="pc-icon">
+                                <use xlink:href="#custom-story"></use>
+                            </svg>
+                        </span>
+                        <span class="pc-mtext">Dashboard</span>
+                    </a>
+                </li>
+
+                <li class="pc-item pc-caption">
+                    <label>Administrator</label>
+                </li>
+
+                <li class="pc-item pc-hasmenu">
+                    <a href="#!" class="pc-link">
+                        <span class="pc-micon">
+                            <svg class="pc-icon">
+                                <use xlink:href="#custom-status-up"></use>
+                            </svg>
+                        </span>
+                        <span class="pc-mtext">Users</span>
+                        <span class="pc-arrow"><i data-feather="chevron-right"></i></span>
+                    </a>
+                    <ul class="pc-submenu">
+                        <li class="pc-item"><a class="pc-link" href="../dashboard/finance.html">Administrator Management</a></li>
+                        <li class="pc-item"><a class="pc-link" href="../dashboard/index.html">Client Management</a></li>
+                        <li class="pc-item"><a class="pc-link" href="../dashboard/analytics.html">Tasker Management</a></li>
+                    </ul>
+                </li>
+
+                <li class="pc-item pc-hasmenu">
+                    <a href="#!" class="pc-link">
+                        <span class="pc-micon">
+                            <svg class="pc-icon">
+                                <use xlink:href="#custom-status-up"></use>
+                            </svg>
+                        </span>
+                        <span class="pc-mtext">Services</span>
+                        <span class="pc-arrow"><i data-feather="chevron-right"></i></span>
+                    </a>
+                    <ul class="pc-submenu">
+                        <li class="pc-item"><a class="pc-link" href="../dashboard/finance.html">Services Type </a></li>
+                        <li class="pc-item"><a class="pc-link" href="../dashboard/index.html">Services Management</a></li>
+                    </ul>
+                </li>
+
+            </ul>
+            <!-- SideBar Menu Item End -->
+        </div>
+    </div>
+</nav>
